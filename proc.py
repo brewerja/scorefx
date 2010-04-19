3965df64611c2f28a57c90f24d01547567133c64
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2010-04-19 16:36:59 -0400

    Forgot to turn off the debugging print statements

--- proc.py
+++ proc.py
@@ -170,7 +170,6 @@ class procMLB(saxutils.handler.ContentHandler):
             play = plays["walk"]
             out = False
         elif word == "grounds" :
-            print action
             mtch = re.search("grounds out.*?, (\w*)", action)
             if mtch :
                 play = plays["ground"] + positions[mtch.group(1)]
