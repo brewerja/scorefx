154875979fe208e892b2d54a39e2dd82f48fe030
John Brewer <brewerja@gmail.com>
2018-08-12 23:25:59 -0400

    Refactor driver.py

--- driver.py
+++ driver.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import json
 
 from datetime import datetime, timedelta
 from urllib2 import urlopen
@@ -26,59 +27,38 @@ class Game:
         self.home = home
         self.away = away
 
-    def jsonString(self):
-        ret = '{"url": "%s", "home": "%s", away: "%s"}' % (self.url, self.home,
-                                                           self.away)
-        return ret
 
+def getGames(date):
+    url = ('http://gd2.mlb.com/components/game/mlb/year_%04d/month_%02d/'
+           'day_%02d') % (date.year, date.month, date.day)
 
-def getGames(gameDate):
-        year = gameDate.strftime('%Y')
-        month = gameDate.strftime('%m')
-        date = gameDate.strftime('%d')
+    data = urlopen(url).read()
 
-        url = ('http://gd2.mlb.com/components/game/mlb/year_%s/month_%s/'
-               'day_%s') % (year, month, date)
+    games = []
+    for game in re.findall('href="day_%02d/(gid_%04d_%02d_%02d.*?)"' %
+                           (date.day, date.year, date.month, date.day), data):
+        away = game.split('_')[4][:3]
+        home = game.split('_')[5][:3]
+        if away in TEAMS and home in TEAMS:
+            games.append(Game(game.rstrip('/'), TEAMS[away], TEAMS[home]))
 
-        data = urlopen(url).read()
-
-        games = []
-        for game in re.findall('href="day_%s/(gid_%s_%s_%s.*?)"' %
-                               (date, year, month, date), data):
-            away = game.split('_')[4][:3]
-            if 'mlb' in away:
-                away = away[0:3]
-            home = game.split('_')[5][:3]
-            if 'mlb' in home:
-                home = home[0:3]
-            if away in TEAMS and home in TEAMS:
-                games.append(Game(game.rstrip('/'), TEAMS[away], TEAMS[home]))
-
-        return games
+    return games
 
 
 class GameLister(webapp2.RequestHandler):
     def get(self):
-        # Translate the passed-in args to a datetime object
-        year = self.request.get('year')
-        month = self.request.get('month')
-        day = self.request.get('day')
-        gameDate = datetime.strptime(year + month + day, '%Y%m%d')
+        """Given a year, month, day, return JSON listing of games."""
+        try:
+            year = int(self.request.get('year'))
+            month = int(self.request.get('month'))
+            day = int(self.request.get('day'))
+            date = datetime(year, month, day)
+        except (TypeError, ValueError):
+            self.abort(400)
 
-        # Get the games for the requested day
-        games = getGames(gameDate)
-
-        # Translate the list of games to JSON
-        gamesJSON = '[\n'
-        for game in games:
-            gamesJSON += game.jsonString()
-            gamesJSON += ',\n'
-        gamesJSON = gamesJSON.strip(',\n')
-        gamesJSON += '\n]\n'
-
-        # Return the JSON
-        self.response.headers['Content-Type'] = 'text/javascript'
-        self.response.out.write(gamesJSON)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(
+            json.dumps([g.__dict__ for g in getGames(date)]))
 
 
 class PageBuilder(webapp2.RequestHandler):
@@ -87,19 +67,17 @@ class PageBuilder(webapp2.RequestHandler):
         # If no game has started today, use yesterday.
         # Timezones are screwy in Python, so for now just get UTC and adjust
         # manually.
-        gameDate = datetime.utcnow()
-        gameDate -= timedelta(hours=4)
-        url = ('http://gd2.mlb.com/components/game/mlb/year_%s/month_%s/'
-               'day_%s/master_scoreboard.xml') % (gameDate.strftime('%Y'),
-                                                  gameDate.strftime('%m'),
-                                                  gameDate.strftime('%d'))
+        date = datetime.utcnow() - timedelta(hours=4)
+        url = ('http://gd2.mlb.com/components/game/mlb/year_%04d/month_%02d/'
+               'day_%02d/master_scoreboard.xml' %
+               (date.year, date.month, date.day))
         data = urlopen(url).read()
 
         # We don't bother with parsing XML since we know exactly what we're
         # looking for.
         times = re.finditer(' time="([0-9:]+)".*?ampm="([AP]M)"', data,
                             re.DOTALL)
-        curTime = gameDate.time()
+        curTime = date.time()
         started = False
         for mtch in times:
             t = datetime.strptime(mtch.group(1) + mtch.group(2),
@@ -108,13 +86,12 @@ class PageBuilder(webapp2.RequestHandler):
                 started = True
                 break
         if not started:
-            gameDate -= timedelta(days=1)
+            date -= timedelta(days=1)
 
         # Now that we've settled on a date,
         # figure out what games are being played
-        games = getGames(gameDate)
-        dte = ('new Date(%d, %d, %d)' %
-               (gameDate.year, gameDate.month - 1, gameDate.day))
+        games = getGames(date)
+        dte = ('new Date(%d, %d, %d)' % (date.year, date.month - 1, date.day))
         template_values = {'games': games, 'dte': dte}
         t = jinja_env.get_template('index.html')
         self.response.out.write(t.render(template_values))
@@ -130,15 +107,19 @@ class PlayerLookup(webapp2.RequestHandler):
 
 class BuildScorecard(webapp2.RequestHandler):
     def get(self):
-        year = self.request.get('year')
-        month = self.request.get('month')
-        day = self.request.get('day')
-        gid = self.request.get('gameID')
+        try:
+            year = int(self.request.get('year'))
+            month = int(self.request.get('month'))
+            day = int(self.request.get('day'))
+            datetime(year, month, day)  # Validation
+            gid = self.request.get('gameID')
+        except (TypeError, ValueError):
+            self.abort(400)
 
-        url = ('http://gd2.mlb.com/components/game/mlb/year_%s/month_%s/'
-               'day_%s/%s') % (year, month, day, gid)
+        url = ('http://gd2.mlb.com/components/game/mlb/year_%04d/month_%02d/'
+               'day_%02d/%s') % (year, month, day, gid)
 
-        self.response.headers['Content-type'] = 'image/svg+xml'
+        self.response.headers['Content-Type'] = 'image/svg+xml'
 
         # Figure out which teams are playing.  MLB uses inconsistent codes
         # to represent teams (i.e. nya & nyy for the Yanks) so translate
